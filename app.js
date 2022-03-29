468e5f79672b308d5a538957ffb6cea9637cf9bc
myspace20 <balljoe680@gmail.com>
2022-03-28 22:32:12 -0500

    corrections

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('.form');
-const button = document.querySelectorAll('button');
+const button = document.querySelector('button');
 const bill = document.querySelector('.bill')
 const custom = document.querySelector('#custom')
 const people = document.querySelector('#people')
@@ -12,36 +12,50 @@ const people = document.querySelector('#people')
 // const percent = null;
 // console.log(billvalue)
 
-const buttons = Array.from(button);
+// const buttons = Array.from(button);
 
-buttons.forEach(button =>{
-    button.addEventListener('click', e =>{
-        e.preventDefault();
-        const percent = e.target.value
-        console.log(percent)
-    })
-})
+// buttons.forEach(button =>{
+//     button.addEventListener('click', e =>{
+//         e.preventDefault();
+//         const percent = e.target.value
+//         console.log(percent)
+//     })
+// })
 
 
-
+const billvalue = form.bill.value
 
 
 // console.log(buttons)
 
-form.addEventListener('keyup',e =>{
+form.addEventListener(('keyup'),e =>{
     e.preventDefault();
     const billvalue = form.bill.value
-    console.log(billvalue)
+    // console.log(billvalue)
     const customvalue = form.custom.value
     console.log(customvalue)
     const peoplevalue = form.people.value
     console.log(peoplevalue)
+    form.addEventListener('click', e =>{
+        console.log(billvalue* e.target.value *customvalue)
+    })
+    // const vaalue = e.target.value
+    // // const five = form.five.value
+    // // console.log(five)
+    // // const ten = form.ten.value
+    // // console.log(ten)
+    // // const fifteen = form.fifteen.value
+    // // console.log(fifteen)
+    // // const tweentyFive = form.tweentyFive.value
+    // // console.log(tweentyFive)
+    // // const fifty = form.fifty.
+    // console.log(vaalue)
    
     console.log(peoplevalue*billvalue*customvalue)
     
-    // button.addEventListener('click', e=>{
-    //     e.preventDefault()
-    //     console.log(e.target)
-    // })
+
+
+
 })
    
+console.log(billvalue)
